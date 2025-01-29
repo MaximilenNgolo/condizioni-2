7f8921c14237efb3d29e74576d2397827af6dc91
MaximilenNgolo <NGOLOMAXIMPIA@GMAIL.COM>
2025-01-29 20:25:42 +0100

    Add files via upload

---
+++ minu.js
@@ -0,0 +1,23 @@
+let temp ;
+
+
+if (temp < 20){
+    console.log(` non ci sono piu' le mezze stagioni`);
+    
+} else if ( temp >= 30){
+    console.log(`lu mare, lu sole, lu ientu`);
+    
+    
+} else if (temp < 30){
+    console.log(`mi dia una perroni sudata`);
+    
+} else if( temp < 0) {
+    console.log(`non è tanto il freddo quanto l'umidita`);
+    
+} else if (temp < (-10)){
+    console.log(`copriti... ancora ti raffreddi`);
+    
+} else{
+    console.log(`il tempo è buono`);
+    
+}
