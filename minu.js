412c0c288475d5c7c8923323d06e46c21f9b08c2
MaximilenNgolo <NGOLOMAXIMPIA@GMAIL.COM>
2025-01-30 17:13:42 +0100

    Add files via upload
    condizione2

--- minu.js
+++ minu.js
@@ -1,23 +1,66 @@
-let temp ;
+let temp =prompt(`inserisci la temperatura attuale`)
 
 
-if (temp < 20){
-    console.log(` non ci sono piu' le mezze stagioni`);
-    
-} else if ( temp >= 30){
-    console.log(`lu mare, lu sole, lu ientu`);
-    
-    
-} else if (temp < 30){
-    console.log(`mi dia una perroni sudata`);
-    
-} else if( temp < 0) {
+if (temp <= 0){
     console.log(`non è tanto il freddo quanto l'umidita`);
     
-} else if (temp < (-10)){
+} else if( temp <= -10) {
     console.log(`copriti... ancora ti raffreddi`);
-    
+} else if (temp < 20){
+    console.log(` non ci sono piu' le mezze stagioni`);
+
+} else if (temp < 30){
+    console.log(`mi dia una perroni sudata`);
+
 } else{
-    console.log(`il tempo è buono`);
+    console.log(`lu mare, lu sole, lu ientu`);
     
 }
+
+
+
+
+
+
+let tempo =prompt(`inserisci la temperatura attuale`)
+
+
+switch (true){
+  case tempo < -10 :
+  console.log(`copriti... ancora ti raffreddi`);
+  break
+
+  case  tempo < 0 :
+  console.log(`non è tanto il freddo quanto l'umidita`);
+  break
+  
+  
+  case tempo < 20 :
+  console.log(` non ci sono piu' le mezze stagioni`);
+  break
+  
+  case tempo < 30 :
+  console.log(`mi dia una perroni sudata`);
+  break
+  
+
+       
+       default:
+           console.log(`lu mare, lu sole, lu ientu`);
+           
+
+
+
+} 
+
+
+
+    
+
+
+    
+    
+    
+    
+    
+
